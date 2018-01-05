6ff295affdd92e3b985c676ba8a6ca5b47b86d5b
mgmilton <matt.milton@gmail.com>
2018-01-05 08:42:43 -0700

    adds methods to scrabble

--- scrabble_simulator/lib/scrabble.rb
+++ scrabble_simulator/lib/scrabble.rb
@@ -1,7 +1,20 @@
 class Scrabble
 
   def score(word)
-    1
+    return 0 if word == nil
+    letters = word.split("")
+    letters << "bonus" if letters.length == 7
+    letters.map do |letter|
+      letter_scorer(letter)
+    end.sum
+  end
+
+  def letter_scorer(letter)
+    case point_values[letter.upcase]
+    when nil then 0
+    else
+      point_values[letter.upcase]
+    end
   end
 
   def point_values
@@ -12,7 +25,7 @@ class Scrabble
       "M"=>3, "N"=>1, "O"=>1, "P"=>3,
       "Q"=>10, "R"=>1, "S"=>1, "T"=>1,
       "U"=>1, "V"=>4, "W"=>4, "X"=>8,
-      "Y"=>4, "Z"=>10
+      "Y"=>4, "Z"=>10, "BONUS" => 50
     }
   end
 end
--- scrabble_simulator/test/scrabble_test.rb
+++ scrabble_simulator/test/scrabble_test.rb
@@ -6,8 +6,24 @@ require 'pry'
 
 class ScrabbleTest < Minitest::Test
   def test_it_can_score_a_single_letter
-    skip
     assert_equal 1, Scrabble.new.score("a")
     assert_equal 4, Scrabble.new.score("f")
   end
+
+  def test_it_can_score_a_word
+    assert_equal 6, Scrabble.new.score("nice")
+  end
+
+  def test_it_can_score_an_empty_string
+    assert_equal 0, Scrabble.new.score("")
+  end
+
+  def test_it_can_score_a_nil
+    assert_equal 0, Scrabble.new.score(nil)
+  end
+
+  def test_it_can_score_a_bonus
+    assert_equal 84, Scrabble.new.score("jazzman")
+  end
+
 end
